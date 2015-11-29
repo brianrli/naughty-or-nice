01807e1156e1b423d0c277a10a99ca76a45753b2
Brian Li <brian.rusong.li@gmail.com>
2015-11-29 17:45:04 -0500

    initplayers.py

--- initplayers.py
+++ initplayers.py
@@ -2,6 +2,8 @@ from app import db
 from app.models import Player
 
 # initial test
+db.drop_all()
+db.create_all()
 db.session.add(Player('Brian','brian.li@yale.edu',False,"You are a blessing on this earth."))
 db.session.add(Player('Ben','benjamin.bartolome@yale.edu',True))
 db.session.add(Player('Kevin','kevin.garcia@yale.edu',True,'Sucks to suck.'))

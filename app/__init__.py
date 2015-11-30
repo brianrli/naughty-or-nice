cfe58cf0b53f3438bd86cbcdfacda343a2d6a75e
Brian Li <brian.rusong.li@gmail.com>
2015-11-29 20:59:23 -0500

    new background

--- app/__init__.py
+++ app/__init__.py
@@ -25,11 +25,16 @@ db = SQLAlchemy(app)
 from models import Player
 # db.drop_all()
 # db.create_all()
-db.session.commit()
+# db.session.commit()
 
 # initialize database
 # from initplayers import initialize_db
-# initialize_db()
+# db.create_all()
+# db.session.add(Player('Brian','brian.li@yale.edu',False,"You are a blessing on this earth."))
+# db.session.add(Player('Ben','benjamin.bartolome@yale.edu',True))
+# db.session.add(Player('Kevin','kevin.garcia@yale.edu',True,'Sucks to suck.'))
+# db.session.add(Player('Aimee','aimee.sawyer@yale.edu'))
+# db.session.commit()
 # players = Player.query.all()
 # print players
 
